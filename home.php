75828b2a03a19fa279125106f2093a50193d9b5b
JayasriAR <109610628+JayasriAR@users.noreply.github.com>
2022-07-19 20:34:53 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+if (isset($_SESSION["username"])) {
+    $username = $_SESSION["username"];
+    session_write_close();
+} else {
+    // since the username is not set in session, the user is not-logged-in
+    // he is trying to access this page unauthorized
+    // so let's clear all session variables and redirect him to index
+    session_unset();
+    session_write_close();
+    $url = "./index.php";
+    header("Location: $url");
+}
+
+?>
+<HTML>
+<HEAD>
+<TITLE>Welcome</TITLE>
+<link href="assets/css/phppot-style.css" type="text/css"
+	rel="stylesheet" />
+<link href="assets/css/user-registration.css" type="text/css"
+	rel="stylesheet" />
+</HEAD>
+<BODY style="background-color:#33ffc4;align-content: center;padding: 12px 20px;">
+	<div class="phppot-container">
+		<div class="page-header">
+			<span class="login-signup"><a href="logout.php">Logout</a></span>
+		</div>
+		<div class="page-content"><h3>Welcome To Home Page</h3><br>
+           <h2>Your Name is <?php echo $username;?></h2></div>
+	</div>
+</BODY>
+</HTML>
